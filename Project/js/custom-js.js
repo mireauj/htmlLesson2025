9d3ac1807d5d0328b9c521052800ca7d60559987
mireauj <200349174@student.georgianc.on.ca>
2025-08-06 21:36:47 -0400

    Add files via upload

---
+++ Project/js/custom-js.js
@@ -0,0 +1,7 @@
+// $(document).ready(function () {
+//     $(function() {
+//         $("#head").load("./includes/meta.html");
+//         $("#header").load("./includes/header.html");
+//         $("#footer").load("./includes/footer.html");
+//     });
+// });
